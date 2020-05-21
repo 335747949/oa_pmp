4a50201b2a6442d2516646e0028434e3c5903581
lanwei <335747949@qq.com>
2020-05-21 18:33:54 +0800

    feat：创建日志记录~切面处理类

--- server/src/main/java/com/lanwei/pmp/server/annotation/LogAnnotation.java
+++ server/src/main/java/com/lanwei/pmp/server/annotation/LogAnnotation.java
@@ -1,9 +1,16 @@
 package com.lanwei.pmp.server.annotation;
 
+import java.lang.annotation.*;
+
 /**
  * 日志记录
  * @author lanwei
  * @email 335747949@qq.com
  */
+@Target(ElementType.METHOD)   //修饰的对象范围
+@Retention(RetentionPolicy.RUNTIME)   //元注解，该Annotation被保留的时间长短
+@Documented  //表明这个注释是由 javadoc记录的
 public @interface LogAnnotation {
+
+    String value() default "";
 }
---
+++ server/src/main/java/com/lanwei/pmp/server/aspect/LogAspect.java
@@ -0,0 +1,14 @@
+package com.lanwei.pmp.server.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * 日志任务~切面处理类
+ * @author lanwei
+ * @email 335747949@qq.com
+ */
+@Aspect  //把当前类标识为一个切面供容器读取
+@Component
+public class LogAspect {
+}
